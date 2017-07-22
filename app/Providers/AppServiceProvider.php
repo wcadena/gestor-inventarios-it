fb3123a62dedcc10cadd26b6dd27ed174648ea63
wcadena <wcadena@outlook.com>
2017-07-22 12:15:23 -0500

    Aregla soporte de Base de datos MySql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Dusk\DuskServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       Schema::defaultStringLength(191);
+        
     }
 
     /**
