06e3af823bda8e769e630e0ca569952c55ddd91d
wcadena <wcadena@outlook.com>
2017-07-21 23:08:17 -0500

    autorizacion a medida, por perfiles

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,5 +56,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        //ini autentificacion intermedia
+        'authEmp' => \App\Http\Middleware\AuthEmpleado::class,
+        //fin autentificacion intermedia
     ];
 }
---
+++ app/Http/Middleware/AuthEmpleado.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wcadena
+ * Date: 01/08/2016
+ * Time: 14:18
+ */
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+
+class AuthEmpleado
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function handle($request, Closure $next,$role)
+    {
+
+        $rol_emp = $this->auth->user()->rol;
+        
+        if(!str_contains($role, $rol_emp)){
+            return redirect()->action('HomeController@index')
+                //->route('login')
+                ->with('alert', trans('home.alert1',['name' => $rol_emp,'name2' => $role]));
+        }
+        return $next($request);
+
+    }
+}
