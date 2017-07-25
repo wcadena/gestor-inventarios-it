43dc83a48009480be910cf19abcbd6c3873fd00e
wcadena <wcadena@outlook.com>
2017-07-24 19:06:19 -0500

    actualizacion de oauth2

--- resources/views/oauth2.blade.php
+++ resources/views/oauth2.blade.php
@@ -4,9 +4,9 @@
 
 @section('main-content')
 
-<passport-clients></passport-clients>
-<passport-authorized-clients></passport-authorized-clients>
-<passport-personal-access-tokens></passport-personal-access-tokens>
+  <passport-clients></passport-clients>
+  <passport-authorized-clients></passport-authorized-clients>
+  <passport-personal-access-tokens></passport-personal-access-tokens>
 
 @endsection
 
