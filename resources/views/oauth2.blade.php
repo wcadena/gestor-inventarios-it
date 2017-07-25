b149ae7408851ae05329c69f097977d81c7efbda
wcadena <wcadena@outlook.com>
2017-07-24 19:11:13 -0500

    cambio de auth desde site

--- resources/views/oauth2.blade.php
+++ resources/views/oauth2.blade.php
@@ -3,7 +3,7 @@
 
 
 @section('main-content')
-
+  <h1>Passport</h1>
   <passport-clients></passport-clients>
   <passport-authorized-clients></passport-authorized-clients>
   <passport-personal-access-tokens></passport-personal-access-tokens>
