4877435d254c1253fad30fa6efe9ad01a478f930
wcadena <wcadena@outlook.com>
2017-09-07 20:44:09 -0500

    Aumneto de campo para valor contable

---
+++ database/migrations/2017_09_07_203832_add_contable_to_equipos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddContableToEquiposTable extends Migration
+{
+    public function __construct()
+    {
+        \Illuminate\Support\Facades\DB::getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+    }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('equipos', function (Blueprint $table) {
+            $table->string("codigo_contable")->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('equipos', function (Blueprint $table) {
+            //
+        });
+    }
+}
