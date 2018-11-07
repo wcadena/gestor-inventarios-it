df39edf97142d198e7e4533c9d80ed8ecb9626b7
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2018-11-06 23:12:24 -0500

    se aumenta seeder  de custodios
     arrancar asi:
    `php artisan db:seed --class=ContactosTableSeeder`

---
+++ database/factories/CustodiosFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Custodios::class, function (Faker $faker) {
+    return [
+        'pais'         =>'Ecuador',
+        'ciudad'         =>'QUITO',
+        'direccion'         =>$faker->address,
+        'documentoIdentificacion'         =>$faker->randomNumber($nbDigits = NULL, $strict = false) ,
+        'cargo'         =>$faker->jobTitle,
+        'compania'         =>'Avianca Ec',
+        'telefono'         =>$faker->phoneNumber,
+        'estado'         =>'ACTIVO',
+        'created_at'         =>$faker->date($format = 'Y-m-d', $max = 'now'),
+        'nombre_responsable'         =>$faker->name,
+        'email'         =>$faker->companyEmail,
+    ];
+});
---
+++ database/seeds/ContactosTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ContactosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        if(\App\Empresa::where('empresa' ,'=','Avianca EC')->first() == null ){
+            \Illuminate\Support\Facades\DB::table('empresas')->insert([
+                'empresa' => 'Avianca EC',
+                'formula_codigo' => '/AV[a-zA-Z0-9]{8,10}/',
+            ]);
+        }
+        if(App\Configuracion::withoutGlobalScopes()->where('empresa' ,'=','Avianca EC')->Config('CUSTODIO_BODEGA') == null){
+            \Illuminate\Support\Facades\DB::table('configuracions')->insert([
+                'id'    => 1,
+                'atributo'  => 'CUSTODIO_BODEGA',
+                'tipo' => 'texto',
+                'valores_fuente'    =>  'FIJO',
+                'fijo'  =>  1,
+                'valor' => null,
+                'empresa'   =>  'Avianca Ec'
+            ]);
+        }
+
+        factory(App\Custodios::class, 20)->create();
+        factory(App\User::class, 1)->create();
+    }
+}
