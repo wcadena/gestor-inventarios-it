fca3f0319b90d914286e75c7cd25294633dfea67
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2018-11-30 09:40:07 -0500

    cambio de test para hacerlos mas rapidos

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\Artisan;
 
 trait CreatesApplication
 {
@@ -13,10 +14,54 @@ trait CreatesApplication
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        return self::initialize();
+    }
 
-        $app->make(Kernel::class)->bootstrap();
+    private static $configurationApp = null;
+    public static function initialize(){
 
-        return $app;
+        if(is_null(self::$configurationApp)){
+            $app = require __DIR__.'/../bootstrap/app.php';
+
+            $app->loadEnvironmentFrom('.env.travis');
+
+            $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+            if (config('database.default') == 'sqlite') {
+                $db = app()->make('db');
+                $db->connection()->getPdo()->exec("pragma foreign_keys=1");
+            }
+
+            Artisan::call('migrate');
+            //Artisan::call('db:seed');
+
+            self::$configurationApp = $app;
+            return $app;
+        }
+
+        return self::$configurationApp;
+    }
+
+    public function tearDown()
+    {
+        if ($this->app) {
+            foreach ($this->beforeApplicationDestroyedCallbacks as $callback) {
+                call_user_func($callback);
+            }
+
+        }
+
+        $this->setUpHasRun = false;
+
+        if (property_exists($this, 'serverVariables')) {
+            $this->serverVariables = [];
+        }
+
+        if (class_exists('Mockery')) {
+            Mockery::close();
+        }
+
+        $this->afterApplicationCreatedCallbacks = [];
+        $this->beforeApplicationDestroyedCallbacks = [];
     }
 }
