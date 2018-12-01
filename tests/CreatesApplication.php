5dc7f181ea267deac2c9f55ad83d709a84265e92
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2018-11-30 19:00:28 -0500

    travistest

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -2,7 +2,7 @@
 
 namespace Tests;
 
-use Illuminate\Support\Facades\Artisan;
+use Illuminate\Contracts\Console\Kernel;
 
 trait CreatesApplication
 {
@@ -13,55 +13,10 @@ trait CreatesApplication
      */
     public function createApplication()
     {
-        return self::initialize();
-    }
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-    private static $configurationApp = null;
+        $app->make(Kernel::class)->bootstrap();
 
-    public static function initialize()
-    {
-        if (is_null(self::$configurationApp)) {
-            $app = require __DIR__.'/../bootstrap/app.php';
-
-            $app->loadEnvironmentFrom('.env.travis');
-
-            $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-            if (config('database.default') == 'sqlite') {
-                $db = app()->make('db');
-                $db->connection()->getPdo()->exec('pragma foreign_keys=1');
-            }
-
-            Artisan::call('migrate');
-            //Artisan::call('db:seed');
-
-            self::$configurationApp = $app;
-
-            return $app;
-        }
-
-        return self::$configurationApp;
-    }
-
-    public function tearDown()
-    {
-        if ($this->app) {
-            foreach ($this->beforeApplicationDestroyedCallbacks as $callback) {
-                call_user_func($callback);
-            }
-        }
-
-        $this->setUpHasRun = false;
-
-        if (property_exists($this, 'serverVariables')) {
-            $this->serverVariables = [];
-        }
-
-        if (class_exists('Mockery')) {
-            \Mockery::close();
-        }
-
-        $this->afterApplicationCreatedCallbacks = [];
-        $this->beforeApplicationDestroyedCallbacks = [];
+        return $app;
     }
 }
