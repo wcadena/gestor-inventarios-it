52cdcf42e13e64bbd7a6683d73b1122ec70e348a
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2018-12-13 13:55:28 -0500

    primer_npm run dev actualizado

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 
