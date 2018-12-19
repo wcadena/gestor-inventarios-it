65bb3a43df0380b05521c8eb8e7a24f112e03240
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2018-12-19 13:28:40 -0500

    config aiiro/laravel-factory-generator

---
+++ config/factory-generator.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /**
+     * Set the namespace of the model.
+     */
+    'namespace' => [
+        'model' => 'App',
+    ],
+
+    /**
+     * List of the columns that will not appear in the factory.
+     */
+    'ignored_columns' => [
+        'id',
+    ],
+];
