0b217998f8446efe924d5bfda636abc819f85bad
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2018-12-25 18:25:10 -0500

    test1

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const { mix } = require('laravel-mix');
 const workboxPlugin = require('workbox-webpack-plugin');
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -57,27 +59,30 @@ mix.js('resources/assets/js/app.js', 'html/js')
   .webpackConfig({
     plugins: [
       new workboxPlugin.InjectManifest({
-        swSrc: 'resources/assets/js/sw.js',
-        swDest: path.join(`${__dirname}/html`, 'sw.js'),
-        //clientsClaim: true,
-        //skipWaiting: true,
-        /*runtimeCaching: [
+        cacheId: 'pwa',
+        filename: 'service-worker.js',
+        staticFileGlobs: ['public/**/*.{css,eot,svg,ttf,woff,woff2,js,html}'],
+        minify: true,
+        stripPrefix: 'html/',
+        handleFetch: true,
+        dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
+          //and have full support for offline first (example below)
+          '/': ['resources/views/welcome.blade.php','resources/views/home.blade.php'],
+          // '/posts': ['resources/views/posts.blade.php']
+        },
+        staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+        navigateFallback: '/',
+        runtimeCaching: [
           {
-            urlPattern: new RegExp(`${process.env.APP_URL}`),
-            handler: 'staleWhileRevalidate',
-            options: {
-              cacheName: `${process.env.APP_NAME}-${process.env.APP_ENV}`
-            }
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+            handler: 'cacheFirst'
           },
           {
-            urlPattern: new RegExp('https://fonts.(googleapis|gstatic).com'),
-            handler: 'cacheFirst',
-            options: {
-              cacheName: 'google-fonts'
-            }
+            urlPattern: /^https:\/\/www\.thecocktaildb\.com\/images\/media\/drink\/(\w+)\.jpg/,
+            handler: 'cacheFirst'
           }
-        ]*/
-
+        ],
+        // importScripts: ['./js/push_message.js']
       })
     ]
   })
