5876091de43222e56a21d061d66c5bc50888263d
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2019-01-25 23:22:23 -0500

    Pagina de Inicio de Sistema

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -30,7 +30,7 @@ class LoginController extends Controller
      */
     public function showLoginForm()
     {
-        return view('adminlte::auth.login');
+        return view('auth.login');
     }
 
     /**
