7aa62d666641f18f52b1cba176a3a9c9b2db34d2
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2019-04-03 20:07:53 -0500

    se aumenta popup de informes

--- resources/views/directory/proyecto_seccion/index.blade.php
+++ resources/views/directory/proyecto_seccion/index.blade.php
@@ -129,11 +129,11 @@
                                                     {{$informep->informeMantoPrev->requerimiento}}
                                                 </td>
                                                 <td class="text-right" style="width: 100px;">
+                                                    <button type="button" data-toggle="modal" data-target="#modal-block-slideleft" class="js-task-star btn btn-sm btn-link text-success">
+                                                        <i class="far fa-edit fa-fw"></i>
+                                                    </button>
                                                     <button type="button" class="js-task-star btn btn-sm btn-link text-warning">
                                                         <i class="far fa-star fa-fw"></i>
-                                                    </button>
-                                                    <button type="button" class="js-task-remove btn btn-sm btn-link text-danger">
-                                                        <i class="fa fa-times fa-fw"></i>
                                                     </button>
                                                 </td>
                                             </tr>
@@ -153,4 +153,29 @@
         </div>
     </div>
     <!-- END Page Content -->
+    <!-- Slide Left Block Modal -->
+    <div class="modal fade" id="modal-block-slideleft" tabindex="-1" role="dialog" aria-labelledby="modal-block-slideleft" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-slideleft" role="document">
+            <div class="modal-content">
+                <div class="block block-themed block-transparent mb-0">
+                    <div class="block-header bg-primary-dark">
+                        <h3 class="block-title">Modal Title</h3>
+                        <div class="block-options">
+                            <button type="button" class="btn-block-option" data-dismiss="modal" aria-label="Close">
+                                <i class="fa fa-fw fa-times"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="block-content">
+                        <p>Potenti elit lectus augue eget iaculis vitae etiam, ullamcorper etiam bibendum ad feugiat magna accumsan dolor, nibh molestie cras hac ac ad massa, fusce ante convallis ante urna molestie vulputate bibendum tempus ante justo arcu erat accumsan adipiscing risus, libero condimentum venenatis sit nisl nisi ultricies sed, fames aliquet consectetur consequat nostra molestie neque nullam scelerisque neque commodo turpis quisque etiam egestas vulputate massa, curabitur tellus massa venenatis congue dolor enim integer luctus, nisi suscipit gravida fames quis vulputate nisi viverra luctus id leo dictum lorem, inceptos nibh orci.</p>
+                    </div>
+                    <div class="block-content block-content-full text-right bg-light">
+                        <button type="button" class="btn btn-sm btn-light" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">Done</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END Slide Left Block Modal -->
 @endsection
