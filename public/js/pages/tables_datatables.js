4c4d3563cd8a75bdf857d8744cced8b5d60c2910
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2019-10-10 16:53:03 -0500

    se actualiza js ```yarn upgrade --no-bin-links``` y despues ```yarn run prod```

---
+++ public/js/pages/tables_datatables.js
@@ -0,0 +1,202 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/js/pages/tables_datatables.js":
+/*!*************************************************!*\
+  !*** ./resources/js/pages/tables_datatables.js ***!
+  \*************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+/*
+ *  Document   : tables_datatables.js
+ *  Author     : pixelcave
+ *  Description: Custom JS code used in Plugin Init Example Page
+ */
+// DataTables, for more examples you can check out https://www.datatables.net/
+var pageTablesDatatables =
+/*#__PURE__*/
+function () {
+  function pageTablesDatatables() {
+    _classCallCheck(this, pageTablesDatatables);
+  }
+
+  _createClass(pageTablesDatatables, null, [{
+    key: "initDataTables",
+
+    /*
+     * Init DataTables functionality
+     *
+     */
+    value: function initDataTables() {
+      // Override a few default classes
+      jQuery.extend(jQuery.fn.dataTable.ext.classes, {
+        sWrapper: "dataTables_wrapper dt-bootstrap4",
+        sFilterInput: "form-control",
+        sLengthSelect: "form-control"
+      }); // Override a few defaults
+
+      jQuery.extend(true, jQuery.fn.dataTable.defaults, {
+        language: {
+          lengthMenu: "_MENU_",
+          search: "_INPUT_",
+          searchPlaceholder: "Search..",
+          info: "Page <strong>_PAGE_</strong> of <strong>_PAGES_</strong>",
+          paginate: {
+            first: '<i class="fa fa-angle-double-left"></i>',
+            previous: '<i class="fa fa-angle-left"></i>',
+            next: '<i class="fa fa-angle-right"></i>',
+            last: '<i class="fa fa-angle-double-right"></i>'
+          }
+        }
+      }); // Init full DataTable
+
+      jQuery('.js-dataTable-full').dataTable({
+        pageLength: 5,
+        lengthMenu: [[5, 10, 20], [5, 10, 20]],
+        autoWidth: false
+      }); // Init DataTable with Buttons
+
+      jQuery('.js-dataTable-buttons').dataTable({
+        pageLength: 5,
+        lengthMenu: [[5, 10, 20], [5, 10, 20]],
+        autoWidth: false,
+        buttons: [{
+          extend: 'copy',
+          className: 'btn btn-sm btn-primary'
+        }, {
+          extend: 'csv',
+          className: 'btn btn-sm btn-primary'
+        }, {
+          extend: 'print',
+          className: 'btn btn-sm btn-primary'
+        }],
+        dom: "<'row'<'col-sm-12'<'text-center bg-body-light py-2 mb-2'B>>>" + "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>"
+      });
+    }
+    /*
+     * Init functionality
+     *
+     */
+
+  }, {
+    key: "init",
+    value: function init() {
+      this.initDataTables();
+    }
+  }]);
+
+  return pageTablesDatatables;
+}(); // Initialize when page loads
+
+
+jQuery(function () {
+  pageTablesDatatables.init();
+});
+
+/***/ }),
+
+/***/ 2:
+/*!*******************************************************!*\
+  !*** multi ./resources/js/pages/tables_datatables.js ***!
+  \*******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! C:\Users\johnc\Desktop\Projects\Dashmix - Laravel Starter Kit\resources\js\pages\tables_datatables.js */"./resources/js/pages/tables_datatables.js");
+
+
+/***/ })
+
+/******/ });
