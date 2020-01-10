b7929bf04f31567cb4a23335e7db56a170a09551
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2020-01-09 22:00:29 -0500

    actualizacion de datos de aplicacion

--- utils/populate.sql
+++ utils/populate.sql
@@ -259,14 +259,15 @@ INSERT INTO `opciones_check_lists` (`id`, `area_id`, `atributo`, `mandatorio`, `
 (60, 1, 'I. EXPLORER 8 /9', 'NO', 'si-no', NULL, NULL),
 (61, 5, 'I. EXPLORER 10', 'NO', 'si-no', NULL, NULL),
 (63, 5, 'VERSION CLIENTE', 'NO', 'text', NULL, NULL),
-(64, 5, 'CPU', 'NO', 'text', NULL, NULL),
-(65, 5, 'BATERIA', 'NO', 'text', NULL, NULL),
-(66, 5, 'DISCOS', 'NO', 'text', NULL, NULL),
-(67, 5, 'GRAFICOS', 'NO', 'text', NULL, NULL),
-(68, 5, 'REDES', 'NO', 'si-no', NULL, NULL),
-(69, 5, 'SISTEMAS OPERATIVOS', 'NO', 'text', NULL, NULL),
-(70, 5, 'INFORMACION DEL SISTEMA', 'NO', 'text', NULL, NULL),
-(71, 5, 'PROGRAMAS INSTALADOS', 'NO', 'text', NULL, NULL);
+(64, 5, 'CPU', 'NO', 'cpu', NULL, NULL),
+(65, 5, 'BATERIA', 'NO', 'bateria', NULL, NULL),
+(66, 5, 'DISCOS', 'NO', 'discos', NULL, NULL),
+(67, 5, 'GRAFICOS', 'NO', 'graficos', NULL, NULL),
+(68, 5, 'REDES', 'NO', 'redes', NULL, NULL),
+(69, 5, 'SISTEMAS OPERATIVOS', 'NO', 'sistemas_op', NULL, NULL),
+(70, 5, 'INFORMACION DEL SISTEMA', 'NO', 'info_sistemas', NULL, NULL),
+(71, 5, 'RAM', 'NO', 'ram', NULL, NULL),
+(72, 5, 'PROGRAMAS INSTALADOS', 'NO', 'programas', NULL, NULL);
 
 
 INSERT INTO `migrations` (`migration`, `batch`) VALUES
