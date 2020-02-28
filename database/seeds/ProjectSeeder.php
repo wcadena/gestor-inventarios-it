af32340930968469bb5bac5f2057ad370eaa00ac
wcadena <wcadena@outlook.com>
2020-02-28 14:45:35 +0000

    Apply fixes from StyleCI

--- database/seeds/ProjectSeeder.php
+++ database/seeds/ProjectSeeder.php
@@ -32,16 +32,16 @@ class ProjectSeeder extends Seeder
         ]);
 
         \Illuminate\Support\Facades\DB::table('projects')->insert([
-            'name'                         => $kj2 = $faker->word,
-            'status'                         => $faker->randomElement($array = ['Ongoing','Finished','OnHold']),
+            'name'                             => $kj2 = $faker->word,
+            'status'                           => $faker->randomElement($array = ['Ongoing', 'Finished', 'OnHold']),
             'description'                      => $faker->text,
-            'start_date'                      => \Carbon\Carbon::now(),
-            'end_date'                      => $faker->date($format = 'Y-m-d', $min = 'now'),
-            'budget'                      => $faker->numberBetween(1000,1000*100),
-            'workspace'                      => \App\Project\Workspace::inRandomOrder()->first()->id,
-            'created_by'                      => App\User::inRandomOrder()->first()->id,
-            'created_at'                      => $faker->date($format = 'Y-m-d', $max = 'now'),
-            'updated_at'                      => $faker->date($format = 'Y-m-d'),
+            'start_date'                       => \Carbon\Carbon::now(),
+            'end_date'                         => $faker->date($format = 'Y-m-d', $min = 'now'),
+            'budget'                           => $faker->numberBetween(1000, 1000 * 100),
+            'workspace'                        => \App\Project\Workspace::inRandomOrder()->first()->id,
+            'created_by'                       => App\User::inRandomOrder()->first()->id,
+            'created_at'                       => $faker->date($format = 'Y-m-d', $max = 'now'),
+            'updated_at'                       => $faker->date($format = 'Y-m-d'),
         ]);
     }
 }
