5d5209e3cb4bf569fa61cf8c692dc23669b8c4ef
Wágner Alexánder Cadena Lastra <wcadena@outlook.com>
2020-02-29 10:50:34 -0500

    Se mejora seeders para pruebas

--- database/seeds/ProjectSeeder.php
+++ database/seeds/ProjectSeeder.php
@@ -12,41 +12,76 @@ class ProjectSeeder extends Seeder
     public function run()
     {
         \Illuminate\Support\Facades\Auth::login(App\User::inRandomOrder()->first());
-        $faker = \Faker\Factory::create();
 
-        factory(\App\Project\Workspace::class)->make()->save();
-        factory(\App\Project\Workspace::class)->make()->save();
+        $workspace = factory(\App\Project\Workspace::class)->make();
+        $workspace->save();
 
-        factory(\App\Project\UserWorkspace::class)->make()->save();
+        $client1 = factory(\App\Project\Client::class)->make();
+        $client1->save();
+        $client2 = factory(\App\Project\Client::class)->make();
+        $client2->save();
 
-        $projects = factory(\App\Project\Project::class, 6)->make();
-        foreach ($projects as $project) {
-            $project->save();
+        $users = \App\User::all();
+
+        foreach ($users as $user){
+            factory(\App\Project\UserWorkspace::class)->make(
+                [
+                    'workspace_id' => $workspace->id,
+                    'user_id' => $user->id,
+                ]
+            )->save();
+
+            $projects = factory(\App\Project\Project::class, 6)->make(
+                [
+                    'workspace'  => $workspace->id,
+                ]
+            );
+            foreach ($projects as $project) {
+                $project->save();
+                factory(\App\Project\ClientProject::class)->make(
+                    [
+                        'client_id' =>$client1->id,
+                        'project_id'=>$project->id
+                    ]
+                )->save();
+                factory(\App\Project\ClientProject::class)->make(
+                    [
+                        'client_id' =>$client2->id,
+                        'project_id'=>$project->id
+                    ]
+                )->save();
+                factory(\App\Project\UserProject::class)->make(
+                    [
+                        'user_id' => $user->id,
+                        'project_id'=>$project->id
+                    ]
+                )->save();
+
+                $milestone=factory(\App\Project\Milestone::class)->make();
+                $milestone->save();
+                $tasks = factory(\App\Project\Task::class, 5)->make(
+                    [
+                        'project_id'=>$project->id,
+                        'milestone_id' => $milestone->id,
+                    ]
+                );
+
+                foreach ($tasks as $task) {
+                    $task->save();
+                    $subtasks = factory(\App\Project\SubTask::class, 7)->make([
+                        'task_id' => $task->id,
+                    ]);
+                    foreach ($subtasks as $subtask) {
+                        $subtask->save();
+                    }
+                }
+            }
         }
-
-        $clients = factory(\App\Project\Client::class, 6)->make();
-        foreach ($clients as $client) {
-            $client->save();
-        }
-
-        factory(\App\Project\ClientProject::class)->make()->save();
-        factory(\App\Project\Milestone::class)->make()->save();
 
         factory(\App\Project\Note::class)->make()->save();
         $UserProjects = factory(\App\Project\UserProject::class, 2)->make();
         foreach ($UserProjects as $UserProject) {
             $UserProject->save();
-        }
-
-        $tasks = factory(\App\Project\Task::class, 5)->make();
-        foreach ($tasks as $task) {
-            $task->save();
-            $subtasks = factory(\App\Project\SubTask::class, 7)->make([
-                'task_id' => $task->id,
-            ]);
-            foreach ($subtasks as $subtask) {
-                $subtask->save();
-            }
         }
     }
 }
