7308d92377bfd1813d7d3b68eed9f67c6bd3ad82
wcadena <wcadena@outlook.com>
2021-02-16 16:39:17 -0500

    Se coloca Coors y funciona en sistema

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -28,7 +28,6 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
-            'cors',
             'signature:X-Application-Name',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
