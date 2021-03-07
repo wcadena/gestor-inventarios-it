88f54b7e62271d76c0f8cfbe09675f769c8422b5
wcadena <wcadena@outlook.com>
2021-03-06 21:50:39 -0500

    se actualiza captura de informacion de equipos desde aplicacion de escritorio

--- app/Http/Controllers/api/CheckList_OpcionesCheckListController.php
+++ app/Http/Controllers/api/CheckList_OpcionesCheckListController.php
@@ -88,56 +88,40 @@ class CheckList_OpcionesCheckListController extends ApiController
         Storage::disk('local')->put($hash_p.'.txt', $file);
         $sistema    = json_decode($file);
         $equipo = Equipos::where('no_serie', 'like', '%'.$request->no_serie.'%')->firstOrFail();
-
-        //dd($sistema->programas[0]);
-
+        $check_list_id = $equipo->check_listxc->id;
+        $utilssacs = CheckList_OpcionesCheckList::where('check_list_id', $check_list_id)->get();
+        foreach ($utilssacs as $utilssac) {
+            $utilssac->delete();
+        }
+//delete from check_list__opciones_check_lists where check_list_id = 1 and opciones_check_list_id is null;
+        foreach ($sistema->programas as $key=>$value) {
+            $this->add_CheckList_OpcionesCheckList($check_list_id,'programas',$key,$value );
+        }
         foreach ($sistema->sistema as $key=>$value){
-            $campos = [];
-            $campos['check_list_id']    = $equipo->check_listxc->id;
             if(in_array($key ,['version','node','v8','inetLatency'])){
-                $opcion = OpcionesCheckList::findOrFail($this->buscaOpcion($key));
-                $campos['valor1']                   = $value;
-                $campos['valor2']                   = $key;
-                $campos['valor3']                   = $key;
-                $campos['tipo']                     = $opcion->tipo;
-                $campos['atributo']                 = $opcion->atributo;
-                $campos['opciones_check_list_id']   = $opcion->id;
-                CheckList_OpcionesCheckList::create($campos);
+                $this->add_CheckList_OpcionesCheckList($check_list_id,$key,$key,$value );
             }elseif(in_array($key, ['system','bios','baseboard','chassis','os','uuid','versions','cpu','graphics','net','memLayout','diskLayout','time','cpuCurrentSpeed','services','currentLoad','networkConnections','fsSize','battery','temp','mem','wifiNetworks','users','processes','networkStats'])){
                 foreach ($value as $key_sub=>$value_sub) {
                     if(!is_array($value_sub) && !is_object($value_sub)){
-                        $opcion = OpcionesCheckList::findOrFail($this->buscaOpcion($key));
-                        $campos['valor1']                   = $value_sub;
-                        $campos['valor2']                   = $key_sub;
-                        $campos['valor3']                   = $key;
-                        $campos['tipo']                     = $opcion->tipo;
-                        $campos['atributo']                 = $opcion->atributo;
-                        $campos['opciones_check_list_id']   = $opcion->id;
-                        CheckList_OpcionesCheckList::create($campos);
+                        $this->add_CheckList_OpcionesCheckList($check_list_id,$key,$key_sub,$value_sub );
                     }else{
                         foreach ($value_sub as $key_sub2=>$value_sub2) {
-                            //$this->add_CheckList_OpcionesCheckList($key,$key_sub.'.'.$key_sub2,$value_sub2 );
-                            $opcion = OpcionesCheckList::findOrFail($this->buscaOpcion($key));
-                            $campos['valor1']                   = json_encode($value_sub2);
-                            $campos['valor2']                   = $key_sub.'.'.$key_sub2;
-                            $campos['valor3']                   = $key;
-                            $campos['tipo']                     = $opcion->tipo;
-                            $campos['atributo']                 = $opcion->atributo;
-                            $campos['opciones_check_list_id']   = $opcion->id;
-                            CheckList_OpcionesCheckList::create($campos);
+                            $this->add_CheckList_OpcionesCheckList($check_list_id,$key,$key_sub.'.'.$key_sub2,$value_sub2 );
                         }
                     }
                 }
             }else{
                 // return json_encode($sistema->sistema);
-                dd($sistema->sistema,$key,$value);
+                // dd($sistema->sistema,$key,$value);
             }
 
         }
-        return $this->showMessage('Hola', 201);
+        return response()->json(['success' => true]);
     }
-    private function add_CheckList_OpcionesCheckList($key,$key_sub,$value_sub2){
-        $opcion = OpcionesCheckList::findOrFail($this->buscaOpcion($key));
+    private function add_CheckList_OpcionesCheckList($check_list_id,$key,$key_sub,$value_sub2,$keydata = null){
+        $campos                             = [];
+        $campos['check_list_id']            = $check_list_id;
+        $opcion                             = OpcionesCheckList::findOrFail($this->buscaOpcion($key));
         $campos['valor1']                   = json_encode($value_sub2);
         $campos['valor2']                   = $key_sub;
         $campos['valor3']                   = $key;
@@ -165,57 +149,36 @@ class CheckList_OpcionesCheckListController extends ApiController
                 return 79;
             case 'cpu':
                 return 64;
+            case 'battery':
+                return 65;
+            case 'fsSize':
+            case 'diskLayout':
+                return 66;
             case 'graphics':
                 return 67;
             case 'net':
+            case 'networkConnections':
+            case 'wifiNetworks':
+            case 'networkStats':
+            case 'inetLatency':
                 return 68;
             case 'memLayout':
             case 'mem':
                 return 71;
-            case 'diskLayout':
-                return 66;
-            case 'time':
-                return 74;
             case 'node':
+            case 'programas':
                 return 72;
             case 'version':
                 return 73;
             case 'v8':
-                return 74;
+            case 'time':
             case 'cpuCurrentSpeed':
-                return 74;
             case 'services':
-                return 74;
             case 'currentLoad':
             case 'temp':
                 return 74;
-            case 'networkConnections':
-            case 'wifiNetworks':
-            case 'networkStats':
-                return 68;
-            case 'inetLatency':
-                return 68;
-            case 'fsSize':
-                return 66;
-            case 'battery':
-                return 65;
             case 'users':
                 return 82;
-            case 2:
-                return 74;
-            case 2:
-                return 74;
-            case 2:
-                return 74;
-            case 2:
-                return 74;
-            case 2:
-                return 74;
-            case 2:
-                return 74;
-            case 2:
-                return 74;
-
 
         }
     }
@@ -233,18 +196,20 @@ class CheckList_OpcionesCheckListController extends ApiController
         return $this->showOne($checkList_OpcionesCheckList);
     }
 
+    private function borrartipo_master($opciones_check_list_id,$check_list_id){
+        $utilssacs = CheckList_OpcionesCheckList::where('opciones_check_list_id', $opciones_check_list_id)
+            ->where('check_list_id', $check_list_id)->get();
+        foreach ($utilssacs as $utilssac) {
+            $utilssac->delete();
+        }
+    }
     public function borrartipo(Request $request)
     {
         $request->validate([
             'opciones_check_list_id'     => 'required',
             'check_list_id'              => 'required',
         ]);
-        $utilssacs = CheckList_OpcionesCheckList::where('opciones_check_list_id', $request->opciones_check_list_id)
-            ->where('check_list_id', $request->check_list_id)->get();
-        foreach ($utilssacs as $utilssac) {
-            $utilssac->delete();
-        }
-
+        $this->borrartipo_master($request->opciones_check_list_id,$request->check_list_id);
         return response()->json(['success' => true]);
     }
 }
